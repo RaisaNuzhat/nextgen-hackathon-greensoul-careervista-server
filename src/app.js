131aad74b68b00fe21bbacce17a48ef59a425b37
Mahfuja <bajibaji1004@gmail.com>
2025-11-13 22:52:38 +0600

    profile,update profile done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import skillsRoutes from "./routes/skillsRoutes.js";
 import { connectDB } from "./config/database.js";
 import { corsMiddleware } from "./middlewares/corsMiddleware.js";
 const app = express();
@@ -16,6 +17,7 @@ await connectDB(); // top-level await
 // Routes
 app.use("/api", userRoutes);
 app.use("/api", authRoutes);
+app.use("/api", skillsRoutes);
 
 // app.use("/api/jobs", jobRoutes);
 // app.use("/api/resources", resourceRoutes);
